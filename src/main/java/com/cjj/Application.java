414960c2ea8f177eaf1960952e12153469de22f0
littlecar <xc_chejj@163.com>
2019-09-02 17:23:49 +0800

    20190902 Initial commit

---
+++ src/main/java/com/cjj/Application.java
@@ -0,0 +1,17 @@
+package com.cjj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 描述：
+ *
+ * @author littlecar
+ * @date 2019/9/2 17:14
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
